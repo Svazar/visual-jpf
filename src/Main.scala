315bc03907c683297f5490ef164e0fce0768657e
Dmitry Leskov <dleskov@excelsior-usa.com>
2017-10-13 11:49:13 +0700

    First UI prototype

--- src/Main.scala
+++ src/Main.scala
@@ -2,36 +2,64 @@ import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.beans.property.{ObjectProperty, ReadOnlyStringWrapper, StringProperty}
 import scalafx.scene.Scene
-import scalafx.scene.control.{TreeTableColumn, TreeTableView}
+import scalafx.scene.control.{TreeItem, TreeTableColumn, TreeTableView}
 
-case class Data(s: StringProperty) {
-  def this(_s: String) = this(StringProperty(_s))
+case class ThreadData(n: Int, s: StringProperty) {
+  def this(_n: Int, _s: String) = this(_n, StringProperty(_s))
 }
 
-class ThreadColumn extends TreeTableColumn[Data, String]("Thread") {
+class ControlColumn extends TreeTableColumn[ThreadData, String]("View") {
+  editable = false
+  minWidth = 50
+  cellValueFactory = { p => ReadOnlyStringWrapper("-")}
+}
+
+class ThreadColumn(val n: Int) extends TreeTableColumn[ThreadData, String]("Thread " + n) {
   editable = false
   minWidth = 100
-  cellValueFactory = { p => ReadOnlyStringWrapper(p.value.value.value.s())}
+  cellValueFactory = { p => ReadOnlyStringWrapper(if (n == p.value.value.value.n) p.value.value.value.s() else "")}
 }
 
 object Main extends JFXApp {
 
   val data = Seq(
-    new Data("a"),
-    new Data("b"),
-    new Data("c")
+    new ThreadData(0, "t0-0"),
+    new ThreadData(0, "t0-1"),
+    new ThreadData(1, "t1-0")
   )
+
+  def group(data: Seq[ThreadData]): Seq[Seq[ThreadData]] = {
+    if (data.isEmpty) Seq()
+    else {
+      val (h, t) = data.span(_.n == data.head.n)
+      h +: group(t)
+    }
+  }
+
+  val rootnode = new TreeItem(new ThreadData(0, "all")) {
+    expanded = true
+//    children = data.map(new TreeItem(_))
+    children = group(data).map(g => {
+      val n = new TreeItem(g.head) {
+        if (!g.tail.isEmpty) {
+          children = g.tail.map(new TreeItem(_))
+        }
+      }
+      n
+    })
+  }
 
   stage = new PrimaryStage {
     title = "Visual JPF"
     scene = new Scene(1024, 768) {
-      root = new TreeTableView[Data] {
+      root = new TreeTableView[ThreadData](rootnode) {
         columns ++= Seq(
-          new ThreadColumn
+          new ControlColumn,
+          new ThreadColumn(0),
+          new ThreadColumn(1),
+          new ThreadColumn(2)
         )
       }
     }
   }
-
-
 }
